90a36127ade5710d2729b7f36aad8119afccb40e
Samucker <zsamuel513@gmail.com>
2024-09-23 21:11:37 -0400

    Se corrigio el puzzle y se culmino la primera y segunda etapa del nivel de seleccion

--- Scripts/Escena-adivinarsonido1.gd
+++ Scripts/Escena-adivinarsonido1.gd
@@ -2,26 +2,49 @@ extends Node2D
 
 var alreadyClicked = false
 var canClickAgain = false
+var correctAreaPressed = false
+
 signal node_pressed
 
 @onready var button1 = $Button
 @onready var button2 = $Button2
+@onready var audioInicial = $AudioInicial
+@onready var audioReady = $AudioReady  # Add this line to reference the new audio
 
 func _ready():
 	for child in get_children():
 		if child is Area2D: 
 			child.connect("node_pressed", _on_node_pressed)
+			child.connect("correct_area_pressed", _on_correct_area_pressed)  
+	_on_ready()
 
-
+func _on_ready():
+	for child in get_children():
+		if child is Area2D:
+			child.input_pickable = false  # Deshabilitar la entrada de los Area2D
+	audioInicial.play() 
+	await audioInicial.finished  
+	audioReady.play() 
+	await audioReady.finished
+	for child in get_children():
+		if child is Area2D:
+			child.input_pickable = true  # Habilitar la entrada de los Area2D después de que termine el audio
 
 func _on_node_pressed():
-
 	for child in get_children():
 		if child is Area2D:
 			child.input_pickable = false 
+			audioReady.stop()  # Stop the ready audio
 
 func _on_button_pressed():
-
+	if correctAreaPressed:  
+		print("Changing scene...")
+		$Correct.play()
+		await get_tree().create_timer(2.0).timeout
+		get_tree().change_scene_to_file("res://Escenas/EscenaPrincipal.tscn")
+	else:
+		print("Incorrect Area2D pressed")
+		$error2.play()
 	for child in get_children():
 		if child is Area2D:
 			child.input_pickable = true 
@@ -29,8 +52,12 @@ func _on_button_pressed():
 			button2.visible = false
 
 func _on_button_2_pressed():
+	$error.play()
 	for child in get_children():
 		if child is Area2D:
 			child.input_pickable = true 
 			button1.visible = false
 			button2.visible = false
+
+func _on_correct_area_pressed():
+	correctAreaPressed = true 
--- Scripts/Sonido1.gd
+++ Scripts/Sonido1.gd
@@ -1,5 +1,6 @@
 extends Area2D
 signal node_pressed
+signal correct_area_pressed
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
@@ -9,4 +10,4 @@ func _input_event(viewport, event, shape_idx):
 		await get_tree().create_timer(4.0).timeout
 		$"../Button".visible = true
 		$"../Button2".visible = true
-
+		emit_signal("correct_area_pressed")  # Add this line to send the extra signal
